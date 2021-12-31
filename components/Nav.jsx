45dab6d03a0347e6962578eb4a6a26a188ad11d2
Victor Manuel Velazquez Fuentes <victor.velazfuentez@gmail.com>
2021-12-31 17:48:01 -0600

    next js

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,20 +1,7 @@
 import Link from 'next/link';
-import NProgress from "nprogress";
-import { useEffect } from 'react'
-import router, { useRouter } from "next/router";
 
 
 const Nav = () => {
-    useEffect(() => {
-        const handleRouteChange = url => {
-            NProgress.start();
-        }
-        router.events.on('routeChangeStart', handleRouteChange)
-        router.events.on("routeChangeComplete", () => NProgress.done());
-        return () => {
-            router.events.off('routeChangeStart', handleRouteChange)
-        }
-    }, [])
 
     return (
         <nav className='bg-secondary p-3 mb-4' style={{ borderRadius: '0.5rem' }}>
